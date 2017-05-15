0b386318572e54e3bc003ffa5641c1b4d0d81ed9
Erik Clarke <erikclarke@fastmail.com>
2017-05-15 16:28:38 -0600

    two-step process

--- src/main.rs
+++ src/main.rs
@@ -2,109 +2,148 @@ extern crate bio;
 extern crate clap;
 extern crate fnv;
 extern crate itertools;
+extern crate rayon;
 
 use std::vec::Vec;
-use std::fmt::{self, Display};
-use std::io;
+use std::io::{self, BufRead};
+use std::ops::Div;
+use std::cmp::Ordering;
 
 use bio::alphabets;
 use bio::alphabets::RankTransform;
 use bio::io::fastq;
 
-use clap::{Arg, App};
-
-use itertools::Itertools;
+use clap::{App, Arg, ArgMatches};
 
 use fnv::FnvHashSet;
 
 fn main() {
 
-    let cli = App::new("Unique Kmers")
-        .arg(Arg::with_name("input")
-                 .short("i")
-                 .long("input")
-                 .default_value("-")
-                 .takes_value(true)
-                 .help("Path to FASTQ file (default: read from stdin)"))
-        .arg(Arg::with_name("min_k")
-                 .short("m")
-                 .long("min_k")
-                 .default_value("3")
-                 .takes_value(true)
-                 .help("Smallest k to search"))
-        .arg(Arg::with_name("max_k")
-                 .short("M")
-                 .long("max_k")
-                 .default_value("7")
-                 .takes_value(true)
-                 .help("Largest k to search"))
+    let args = App::new("Measure/filter sequence complexity based on unique k-mers")
+        .subcommand(App::new("calculate")
+            .arg(Arg::with_name("k")
+                .short("k")
+                .takes_value(true)
+                .help("length of k-mer to use")
+                .default_value("4")))
+        .subcommand(App::new("filter")
+            .arg(Arg::with_name("threshold")
+                .short("t")
+                .long("threshold")
+                .takes_value(true)
+                .help("Minimum z-score to keep")
+                .default_value("-1.5"))
+            .arg(Arg::with_name("invert")
+                .long("invert")
+                .takes_value(false)
+                .help("Keep seqs lower than threshold (for debugging)")))
         .get_matches();
 
-    let seq_fp = cli.value_of("input").unwrap();
-    let min_k: u32 = cli.value_of("min_k")
-        .unwrap()
-        .trim()
-        .parse()
-        .expect("Need an integer!");
-    let max_k: u32 = cli.value_of("max_k")
-        .unwrap()
+    match args.subcommand() {
+        ("calculate", Some(subargs)) => calculate(subargs),
+        ("filter", Some(subargs)) => filter(subargs),
+        (_, _) => {println!("{}",args.usage()); std::process::exit(1);}
+    }
+}
+
+fn calculate(args: &ArgMatches) {
+
+    let k: u32 = args
+        .value_of("k")
+        .expect("missing k!")
         .trim()
         .parse()
         .expect("Need an integer!");
 
-    let fq_records = read_fq(seq_fp);
+    let fq_records = fastq::Reader::new(io::stdin()).records();
     let alphabet = alphabets::dna::iupac_alphabet();
     let rank = RankTransform::new(&alphabet);
 
-    fq_records
+    let freqs: Vec<KFrequencies> = fq_records
         .map(|r| r.expect("Error reading FASTQ record"))
-        .map(|r| {
-                 println!("{}", UniqueKs::new(min_k, max_k, &rank)
-                     .count(r.seq())
-                     .tabular("\t"))
-             }).collect::<Vec<()>>();
+        .map(|r| KFrequencies::new(k, &rank, r))
+        .collect();
+
+    freqs
+        .iter()
+        .map(|kf| println!("{}\t{:.4}", kf.id, kf.score))
+        .collect::<Vec<()>>();
+
 }
 
-fn read_fq(path: &str) -> Box<Iterator<Item = Result<fastq::Record, std::io::Error>>> {
-    match path {
-        "-" => Box::new(fastq::Reader::new(io::stdin()).records()),
-        _ => Box::new(fastq::Reader::from_file(path).expect("Could not open specified file").records())
-    }
-}
+fn filter(args: &ArgMatches) {
+    let threshold: f64 = args
+        .value_of("threshold")
+        .expect("missing threshold!")
+        .trim()
+        .parse()
+        .expect("Need a numeric value!");
 
-struct UniqueKs<'a> {
-    min_k: u32,
-    max_k: u32,
-    rank: &'a RankTransform,
-    freqs: Vec<u32>,
-}
+    let invert: bool = args.is_present("invert");
 
-impl<'a> UniqueKs<'a> {
-    fn new(min_k: u32, max_k: u32, rank: &'a RankTransform) -> Self {
-        UniqueKs {
-            min_k,
-            max_k,
-            rank,
-            freqs: Vec::new(),
+    let mut input: Vec<Score> = Vec::new();
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(s) => input.push(Score::new(s)),
+            Err(_) => break,
         }
-    }
+    };
 
-    fn count(&self, text: &[u8]) -> Self {
-        let mut freqs: Vec<u32> = Vec::new();
-        for k in self.min_k..self.max_k {
-            freqs.push(unique_qgrams(text, k, self.rank) as u32);
-        }
-        UniqueKs { freqs, ..*self }
-    }
+    let comp = if invert { Ordering::Less } else { Ordering::Greater};
 
-    fn tabular(&self, sep: &str) -> String {
-        format!("{}", self.freqs.iter().format(sep))
+    let mean: f64 = input.iter()
+        .map(|s| s.score)
+        .sum::<f64>()
+        .div((input.len() as f64));
+    let sd: f64 = input.iter()
+        .map(|s| (s.score - mean).powi(2))
+        .sum::<f64>()
+        .div((input.len() - 1) as f64)
+        .sqrt();
+    input
+        .iter()
+        .filter(|s| ((s.score - mean)/sd).partial_cmp(&threshold) == Some(comp))
+        .map(|s| println!("{}", s.id))
+        .collect::<Vec<()>>();
+}
+
+struct Score {
+    id: String,
+    score: f64,
+}
+
+impl Score {
+    fn new(s: String) -> Self {
+        let parts: Vec<&str> = s.splitn(2, "\t").collect();
+        let id: String = parts[0].trim().to_owned();
+        let score: f64 = parts[1].trim().parse().expect(&format!("Error reading score from {}", s));
+        return Score{ id, score }
     }
 }
 
-impl<'a> Display for UniqueKs<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}, {:?}", self.min_k, self.max_k, self.freqs)
+#[derive(Debug)]
+struct KFrequencies {
+    id: String,
+    k: u32,
+    freq: usize,
+    len: usize,
+    score: f64,
+}
+
+impl KFrequencies {
+    fn new(k: u32, rank: &RankTransform, r: fastq::Record) -> Self {
+        let freq = unique_qgrams(r.seq(), k, rank);
+        let len = r.seq().len();
+        let score = (freq as f64) / (len as f64);
+        KFrequencies {
+            id: r.id().unwrap_or_default().to_owned(),
+            k,
+            freq,
+            len,
+            score,
+        }
     }
 }
 
